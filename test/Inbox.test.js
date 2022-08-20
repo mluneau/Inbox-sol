d1432a17b087ff1afdc0d4e992daeffad3478ce4
Maxence Luneau <maxenceluneau@gmail.com>
2022-08-20 19:57:35 +0200

    Deploy contract test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,57 +3,24 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Constructor function OOP programming
 const web3 = new Web3(ganache.provider()); // Instance of new web3, multiple is possible but mostly using only one
 
-/* 
+const { interface, bytecode } = require('../compile');
 
-MOCHA Testing overview
+let accounts;
+let inbox;
 
-It is a test running framework to test any type of JavaScript code. Small set of fx to understand. 
-3 fx: 
-it: run a test and make an assertion
-describe: groups together 'it' functions
-beforeEach: execute some general setup code
-
-class Car {
-  park() {
-    return 'stopped';
-  }
-
-  drive() {
-    return 'vroom';
-  }
-}
-
-let car ; 
-
-console.log('Initializing test...');
-
-beforeEach(() => {
-  car = new Car();  
-});
-
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  })
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  })
-});
-
-*/
-
-beforeEach(() => {
+beforeEach(async () => {
   // Get a list of all accounts 
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
-
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(inbox);
+  });
+
 });
