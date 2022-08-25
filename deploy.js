2bbc522b0c0c33a4a6f01d736692c7bb0147821b
Maxence Luneau <maxenceluneau@gmail.com>
2022-08-25 11:36:23 +0100

    Update inbox project w/ newer version of solc & libraries

--- deploy.js
+++ deploy.js
@@ -1,26 +1,25 @@
-// deploy code will go here
-
-const HDWallletProvider = require('@truffle/hdwallet-provider');
-const { linkBytecode } = require('solc');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require ('./compile');
-
+ 
+const { abi, evm } = require('./compile');
+ 
 const provider = new HDWallletProvider(
   'pelican swamp slush sell bamboo swear submit warfare lab brief few portion', 'https://rinkeby.infura.io/v3/f3f1cdc7a4364b35b6124fa62865918f'
-  );
+); 
 
-const web3 = new Web3(provider);
-
+  const web3 = new Web3(provider);
+ 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
-  console.log('Attenmpting to deploy from account:', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there !'] })
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
     .send({ gas: '1000000', from: accounts[0] });
-
+ 
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
+ 
 deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,38 +2,38 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // Constructor function OOP programming
 const web3 = new Web3(ganache.provider()); // Instance of new web3, multiple is possible but mostly using only one
-
-const { interface, bytecode } = require('../compile');
-
+ 
+const { abi, evm } = require('../compile');
+ 
 let accounts;
 let inbox;
-let initialString = 'Hi there!';
-
+ 
 beforeEach(async () => {
-  // Get a list of all accounts 
+  // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [initialString] })
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ['Hi there!'],
+    })
     .send({ from: accounts[0], gas: '1000000' });
 });
-
+ 
 describe('Inbox', () => {
+  // console.log(inbox);
+
   it('deploys a contract', () => {
-    // console.log(inbox);
     assert.ok(inbox.options.address);
   });
-
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, initialString);
+    assert.equal(message, 'Hi there!');
   });
-
-  it('modifies the message', async () => {
-    await inbox.methods.setMessage('Test new message').send({ from: accounts[0], gas : '1000000' });
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
     const message = await inbox.methods.message().call();
-    console.log(message);
-    assert.equal(message, 'Test new message');
+    assert.equal(message, 'bye');
   });
 });
