13fcacc6c3359780788cd548581b4becef21062b
Maxence Luneau <maxenceluneau@gmail.com>
2022-08-25 09:35:35 +0100

    creation of deploy.js file

--- deploy.js
+++ deploy.js
@@ -1,1 +1,12 @@
 // deploy code will go here
+
+const HDWallletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecoe } = require ('./conmpile');
+
+const provider = new HDWallletProvider(
+  'pelican swamp slush sell bamboo swear submit warfare lab brief few portion', 
+  https://rinkeby.infura.io/v3/f3f1cdc7a4364b35b6124fa62865918f
+);
+
+const web3 = new Web3(provider);
